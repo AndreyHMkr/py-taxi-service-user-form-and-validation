bf8f5b78a8111aacb8b8049a459c02da9e5acfdd
Andriy <grushevoyandrey@gmail.com>
2025-04-27 18:00:08 +0100

    Fixed some mistakes

--- taxi/views.py
+++ taxi/views.py
@@ -88,6 +88,8 @@ def assign_to_car(request: HttpRequest, pk) -> HttpResponse:
     car = get_object_or_404(Car, pk=pk)
     if not isinstance(car, Car):
         return redirect("taxi:index")
+    if not isinstance(request.user, Driver):
+        return redirect("taxi:index")
     car.drivers.add(request.user)
     return redirect("taxi:car-detail", pk=pk)
 
@@ -96,6 +98,8 @@ def assign_to_car(request: HttpRequest, pk) -> HttpResponse:
 def remove_from_car(request: HttpRequest, pk) -> HttpResponse:
     car = get_object_or_404(Car, pk=pk)
     if not isinstance(car, Car):
+        return redirect("taxi:index")
+    if not isinstance(request.user, Driver):
         return redirect("taxi:index")
     car.drivers.remove(request.user)
     return redirect("taxi:car-detail", pk=pk)
